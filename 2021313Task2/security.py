5e343abc9cf110d4b2b58e5de6a539dd38a617f8
DPainton0912 <dgpainton@gmail.com>
2020-03-12 19:30:47 +1300

    Update security.py

--- 2021313Task2/security.py
+++ 2021313Task2/security.py
@@ -53,7 +53,50 @@ class Security(object):
             path.write_text(ciphertext)
 
     def CaesarDecryptor(self, plaintext):
-        pass
+        shift = int(input("Please enter a number to shift text by: "))
+        ciphertext = ""
+        ucalphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        lcalphabet = "abcdefghijklmnopqrstuvwxyz"
+        numbers = "0123456789"
+        symbols = "!@#$%^&*(),./[]"
+        cipherucalphabet = collections.deque("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        cipherlcalphabet = collections.deque("abcdefghijklmnopqrstuvwxyz")
+        ciphernumbers = collections.deque("0123456789")
+        ciphersymbols = collections.deque("!@#$%^&*(),./[]")
+        cipherucalphabet.rotate(shift)
+        cipherucalphabet.rotate(shift)
+        ciphernumbers.rotate(shift)
+        ciphersymbols.rotate(shift)
+        cipherucalphabet = "".join(list(cipherucalphabet))
+        cipherlcalphabet = "".join(list(cipherlcalphabet))
+        ciphernumbers = "".join(list(ciphernumbers))
+        ciphersymbols = "".join(list(ciphersymbols))
+        i=0
+        for i in range(len(ucalphabet)):
+            if ucalphabet[i] in plaintext:
+                ciphertext = ciphertext + cipherucalphabet[i]
+            i+1
+        for i in range(len(lcalphabet)):
+            if lcalphabet[i] in plaintext:
+                ciphertext = ciphertext + cipherlcalphabet[i]
+            i+1
+        for i in range(len(numbers)):
+            if numbers[i] in plaintext:
+                ciphertext = ciphertext + ciphernumbers[i]
+            i+1
+        for i in range(len(symbols)):
+            if symbols[i] in plaintext and len(symbols)<=16:
+                ciphertext = ciphertext + ciphersymbols[i]
+            i+1
+        filename = input("Please enter a file name: ").strip()
+        if not ".txt" in filename:
+            filename += ".txt"
+        path = Path(filename)
+        if path.exists():
+            path.write_text(ciphertext)
+        else:
+            open(filename, "x")
+            path.write_text(ciphertext)
 
     def PolySubEncryptor(self, plaintext):
         pass
