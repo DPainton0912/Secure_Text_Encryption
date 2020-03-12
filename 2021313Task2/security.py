1e59ce6793b2e3448ddf53b947db7a5f8f3e93f7
DPainton0912 <dgpainton@gmail.com>
2020-03-12 18:36:41 +1300

    Update security.py

--- 2021313Task2/security.py
+++ 2021313Task2/security.py
@@ -13,32 +13,47 @@ class Security(object):
         lcalphabet = "abcdefghijklmnopqrstuvwxyz"
         numbers = "0123456789"
         symbols = "!@#$%^&*(),./[]"
-        cipherucalhabet = collections.deque[ucalphabet]
-        cipherlcalhabet = collections.deque[lcalphabet]
-        ciphernumbers = collections.deque[numbers]
-        ciphersymbols = collections.deque[symbols]
-        cipherucalhabet = cipherucalhabet.rotate(shift)
-        cipherlcalhabet = cipherucalhabet.rotate(shift)
-        ciphernumbers = ciphernumbers.rotate(shift)
-        ciphersymbols = ciphersymbols.rotate(shift)
-        for i in len(plaintext):
-            if ucalphabet in plaintext:
+        cipherucalhabet = collections.deque("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        cipherlcalhabet = collections.deque("abcdefghijklmnopqrstuvwxyz")
+        ciphernumbers = collections.deque("0123456789")
+        ciphersymbols = collections.deque("!@#$%^&*(),./[]")
+        cipherucalhabet.rotate(shift)
+        cipherucalhabet.rotate(shift)
+        ciphernumbers.rotate(shift)
+        ciphersymbols.rotate(shift)
+        cipherucalhabet = "".join(list(cipherucalhabet))
+        cipherlcalhabet = "".join(list(cipherlcalhabet))
+        ciphernumbers = "".join(list(ciphernumbers))
+        ciphersymbols = "".join(list(ciphersymbols))
+        print("cipherucalphabet: "+str(len(cipherucalhabet)))
+        print("plaintext: "+str(len(plaintext)))
+        print("ucalphabet: "+str(len(ucalphabet)))
+        i=0
+        for i in range(len(ucalphabet)):
+            if ucalphabet[i] in plaintext:
                 ciphertext = ciphertext + cipherucalhabet[i]
-            if lcalphabet in plaintext:
+            i+1
+        for i in range(len(lcalphabet)):
+            if lcalphabet[i] in plaintext:
                 ciphertext = ciphertext + cipherlcalhabet[i]
-            if numbers in plaintext:
+            i+1
+        for i in range(len(numbers)):
+            if numbers[i] in plaintext:
                 ciphertext = ciphertext + ciphernumbers[i]
-            if symbols in plaintext:
-                pass
+            i+1
+        for i in range(len(symbols)):
+            if symbols[i] in plaintext and len(symbols)<=16:
+                ciphertext = ciphertext + ciphersymbols[i]
+            i+1
         filename = input("Please enter a file name: ").strip()
         if not ".txt" in filename:
             filename += ".txt"
         path = Path(filename)
         if path.exists():
-            path.write_text()
+            path.write_text(ciphertext)
         else:
             open(filename, "x")
-            path.write_text()
+            path.write_text(ciphertext)
 
     def CaesarDecryptor(self, plaintext):
         pass
