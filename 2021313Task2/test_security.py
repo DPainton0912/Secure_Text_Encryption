2979e9ab7f7ac202c10a73d95b8f6c645b7e67df
DPainton0912 <dgpainton@gmail.com>
2020-03-18 11:35:02 +1300

    Update test_security.py

--- 2021313Task2/test_security.py
+++ 2021313Task2/test_security.py
@@ -1,21 +1,27 @@
 import pytest
 from security import Security
-from io import StringIO
-s = Security()
-shift = StringIO("3\n")
-def test_CaesarEncrypt_Output_Type(monkeypatch):
-    monkeypatch.setattr('sys.stdin', shift)
-    ciphertext = s.CaesarEncryptor("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(),./[]")
+key = 15
+s = Security(key)
+plaintext = """Top 10 Programming Languages 2017 (hackernoon.com)
+1- Python - Python Software Foundation
+2- C - Dennis Ritchie & Bell Labs
+3- Java - Oracle Corporation
+4- C++ - Bell Labs
+5- C# - Microsoft
+6- R - R Core Team
+7- JavaScript - Netscape
+8- PHP - Zend Technologies
+9- Go - Google
+10- Swift - Apple"""
+def test_CaesarEncrypt_Output_Type():
+    ciphertext = s.CaesarEncryptor(plaintext)
     assert type(ciphertext) is str
-def test_CaesarEncrypt_Output(monkeypatch):
-    monkeypatch.setattr('sys.stdin', shift)
-    ciphertext = s.CaesarEncryptor("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(),./[]")
-    assert ciphertext != "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(),./[]"
-def test_CaesarDecrypt_Output_Type(monkeypatch):
-    monkeypatch.setattr('sys.stdin', shift)
-    ciphertext = s.CaesarDecryptor("XYZABCDEFGHIJKLMNOPQRSTUVWxyzabcdefghijklmnopqrstuvw7890123456/[]!@#$%^&*(),.")
+def test_CaesarEncrypt_Output():
+    ciphertext = s.CaesarEncryptor(plaintext)
+    assert ciphertext != plaintext
+def test_CaesarDecrypt_Output_Type():
+    ciphertext = s.CaesarDecryptor(plaintext)
     assert type(ciphertext) is str
-def test_CaesarDecrypt_Output(monkeypatch):
-    monkeypatch.setattr('sys.stdin', shift)
-    ciphertext = s.CaesarDecryptor("XYZABCDEFGHIJKLMNOPQRSTUVWxyzabcdefghijklmnopqrstuvw7890123456/[]!@#$%^&*(),.")
-    assert ciphertext != "XYZABCDEFGHIJKLMNOPQRSTUVWxyzabcdefghijklmnopqrstuvw7890123456/[]!@#$%^&*(),."
+def test_CaesarDecrypt_Output():
+    ciphertext = s.CaesarDecryptor(plaintext)
+    assert ciphertext != plaintext
